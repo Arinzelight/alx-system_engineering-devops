0e0cbd1c1227bf37b36b187893b9b2fe38371bfb
Arinzelight <arinzelight2@gmail.com>
2023-12-22 15:10:16 -0800

    file update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#create a file in /tmp
+# create a file in /tmp
 
 file {'/tmp/scool':
-	content => 'I love puppet',
-	mode 	=> '0744',
-	owner 	=> 'www-data',
-	group 	=> 'www-data',
+  content => 'I love puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 
 }
