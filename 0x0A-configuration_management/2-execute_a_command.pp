356c5e0ba34c4a33ce42f2647e43543e9e136265
Arinzelight <arinzelight2@gmail.com>
2023-12-22 15:38:37 -0800

    process kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # kill process killmenow
 
 exec {'pkill':
-	command => 'pkill killmenow',
-	provider => 'shell',
+  command   => '/usr/bin/pkill killmenow',
+  provider  => 'shell',
+  returns    => [0, 1],
 }
