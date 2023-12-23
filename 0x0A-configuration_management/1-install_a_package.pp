64b8a7f1a1c52d6887e4d65803a9f03a20fe8f11
Arinzelight <arinzelight2@gmail.com>
2023-12-22 16:46:58 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,7 @@
-# Using Puppet, install flask from pip3
-
-# Install pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask using pip3
+# Install Flask using pip3 with specific versions
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.2',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0"',
 }
 
